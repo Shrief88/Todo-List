bf1bb7d1e4fd60e2e0aced0fe15f94e036325662
Shrief Essam <shriefessam1999@gmail.com>
2022-07-23 14:53:07 +0200

    add toggle button functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import Task from './modules/task'
 import Project from './modules/project'
 
 
+const displayController = (()=>{
+    const toggleButton = document.querySelector('#toggle-button');
 
-
-
+    toggleButton.addEventListener('click',()=>{
+        const sidebar = document.querySelector('#sidebar');
+        sidebar.classList.toggle('hide-element')
+    })
+})()
